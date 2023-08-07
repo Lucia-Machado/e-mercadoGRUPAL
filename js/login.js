06f0b202937605124e61c219ea957d3627f850f2
Lucia Machado <lucia.machado.olascoaga@gmail.com>
2023-08-07 17:18:50 -0300

    Ejemplo de validacion

---
+++ js/login.js
@@ -0,0 +1,15 @@
+/*
+
+const form = document.querySelector("form");
+const fname = document.getElementById("fname");
+const lname = document.getElementById("lname");
+const para = document.querySelector("p");
+
+form.addEventListener("submit", (e) => {
+  if (fname.value === "" || lname.value === "") {
+    e.preventDefault();
+    para.textContent = "¡Necesitas completar ambos campos!";
+  }
+});
+
+*/
