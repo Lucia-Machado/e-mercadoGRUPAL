10739d3387d8e32875a4f4d73eb8e87b4ced017d
manuromeyer <manurodriguez2015.11@gmail.com>
2023-08-08 15:19:24 -0300

    validación login

--- js/login.js
+++ js/login.js
@@ -13,3 +13,36 @@ form.addEventListener("submit", (e) => {
 });
 
 */
+document.addEventListener("DOMContentLoaded", function() {
+
+  const email = document.getElementById('email');
+  const pass = document.getElementById('password');
+
+  
+  
+  addEventListener("submit", function validar() {
+    event.preventDefault();
+    if (email.value === "") {
+      Swal.fire({
+        icon: 'error',
+        title: 'Hubo un error...',
+        text: 'Ingrese su email',
+    });
+    }  else if (pass.value === "") {
+      Swal.fire({
+        icon: 'error',
+        title: 'Hubo un error...',
+        text: 'Ingrese su contraseña',
+    });
+    } else {
+      window.location.href = "index.html";
+    }
+  })
+  
+  })
+ 
+
+
+
+
+
