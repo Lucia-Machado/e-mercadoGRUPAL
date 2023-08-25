f1c717580762e36aa79f5c932d6f9de05a2cb07a
VascoEt <arieletchebarne@outlook.com>
2023-08-25 14:18:23 -0300

    arreglo IDs

--- js/products.js
+++ js/products.js
@@ -41,28 +41,19 @@ document.addEventListener("DOMContentLoaded", function (e) {
   let contenidoIndex = localStorage.getItem("EmailPersona");
   let emailPersona = document.getElementById("emailPersona");
   emailPersona.innerHTML = `Perfil: ${contenidoIndex}`;
-});
 
-document.addEventListener("DOMContentLoaded", function (e) {
-  // Recuperar el identificador de categoría almacenado en el almacenamiento local
-  var categoriaSeleccionadaId = localStorage.getItem("catID");
-
+  
+  let categoriaSeleccionadaId = localStorage.getItem("catID");
   if (categoriaSeleccionadaId !== null) {
-      // Construir la URL utilizando la ID de categoría
-      var url = `https://japceibal.github.io/emercado-api/cats_products/${categoriaSeleccionadaId}.json`;
+    let url = `https://japceibal.github.io/emercado-api/cats_products/${categoriaSeleccionadaId}.json`;
 
-      // Obtiene datos JSON de la URL construida
-      getJSONData(url).then(function (resultObj) {
-          if (resultObj.status === "ok") {
-              // Los almacena dentro del ProductsArray
-              ProductsArray = resultObj.data.products;
-              // Llama la función para mostrar la lista de productos
-              showProductsList(ProductsArray);
-          }
-      });
-
-      // Resto de tu código para mostrar el perfil del usuario
-  } else {
-      console.log("Identificador de categoría no encontrado en el almacenamiento local.");
-  }
+    getJSONData(url).then(function (resultObj) {
+      if (resultObj.status === "ok") {
+        ProductsArray = resultObj.data.products;
+        showProductsList(ProductsArray);
+      }
+    });
+  };
 });
+
+
