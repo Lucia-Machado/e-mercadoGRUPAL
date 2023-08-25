567827aaeddea9293a6af43e88126f036f37c299
Lucia Machado <lucia.machado.olascoaga@gmail.com>
2023-08-25 15:30:52 -0300

    kjsdfk

--- js/products.js
+++ js/products.js
@@ -1,6 +1,46 @@
+const ORDER_ASC_BY_NAME = "AZ";
+const ORDER_DESC_BY_NAME = "ZA";
+const ORDER_BY_PRICE_ASC = "PrecioAsc";
+const ORDER_BY_PRICE_DESC = "PrecioDesc";
+const ORDER_BY_SOLD_COUNT = "Vendidos";
+
 const AUTOS_URL =
   "https://japceibal.github.io/emercado-api/cats_products/101.json";
-let ProductsArray = [];
+
+  function sortProducts(criteria, array) {
+    let result = [];
+
+    if (criteria === ORDER_ASC_BY_NAME) {
+        result = array.sort(function (a, b) {
+            if (a.name < b.name) { return -1; }
+            if (a.name > b.name) { return 1; }
+            return 0;
+        });
+    } else if (criteria === ORDER_DESC_BY_NAME) {
+        result = array.sort(function (a, b) {
+            if (a.name > b.name) { return -1; }
+            if (a.name < b.name) { return 1; }
+            return 0;
+        });
+    } else if (criteria === ORDER_BY_PRICE_ASC) {
+        result = array.sort(function (a, b) {
+            return a.cost - b.cost;
+        });
+    } else if (criteria === ORDER_BY_PRICE_DESC) {
+        result = array.sort(function (a, b) {
+            return b.cost - a.cost;
+        });
+    } else if (criteria === ORDER_BY_SOLD_COUNT) {
+        result = array.sort(function (a, b) {
+            return b.soldCount - a.soldCount;
+        });
+    }
+
+    return result;
+}
+
+
+  let ProductsArray = [];
 
 function showProductsList(array) {
   let htmlContentToAppend = "";
@@ -27,15 +67,18 @@ function showProductsList(array) {
   document.getElementById("products-container").innerHTML = htmlContentToAppend;
 }
 
-document.addEventListener("DOMContentLoaded", function (e) {
- 
-  });
+function sortAndShowProducts(sortCriteria) {
+  currentSortCriteria = sortCriteria;
+  ProductsArray = sortProducts(currentSortCriteria, ProductsArray);
+  showProductsList(ProductsArray);
+}
 
+
+document.addEventListener("DOMContentLoaded", function (e) {
   let contenidoIndex = localStorage.getItem("EmailPersona");
   let emailPersona = document.getElementById("emailPersona");
   emailPersona.innerHTML = `Perfil: ${contenidoIndex}`;
 
-  
   let categoriaSeleccionadaId = localStorage.getItem("catID");
   if (categoriaSeleccionadaId !== null) {
     let url = `https://japceibal.github.io/emercado-api/cats_products/${categoriaSeleccionadaId}.json`;
@@ -46,7 +89,26 @@ document.addEventListener("DOMContentLoaded", function (e) {
         showProductsList(ProductsArray);
       }
     });
-  };
+  }
 
+  document.getElementById("sortAsc").addEventListener("click", function () {
+    sortAndShowProducts(ORDER_ASC_BY_NAME);
+});
 
+document.getElementById("sortDesc").addEventListener("click", function () {
+    sortAndShowProducts(ORDER_DESC_BY_NAME);
+});
 
+document.getElementById("sortByPriceAsc").addEventListener("click", function () {
+    sortAndShowProducts(ORDER_BY_PRICE_ASC);
+});
+
+document.getElementById("sortByPriceDesc").addEventListener("click", function () {
+    sortAndShowProducts(ORDER_BY_PRICE_DESC);
+});
+
+document.getElementById("sortBySoldCount").addEventListener("click", function () {
+    sortAndShowProducts(ORDER_BY_SOLD_COUNT);
+});
+
+});
