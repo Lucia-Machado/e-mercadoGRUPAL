269356259ea50f808aad748d84c7b2103c20ac61
MauricioPose <mauriciopose15@gmail.com>
2023-09-02 17:52:59 -0300

    actualizado

--- js/product-info.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded",()=>{
-
-        let contenidoIndex = localStorage.getItem("EmailPersona");
-        let emailPersona = document.getElementById("emailPersona");
-        emailPersona.innerHTML = `Perfil: ${contenidoIndex}`;
-
-})
