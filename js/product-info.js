d39ed62376f7e65cb3f8250d754295b74fa9cffd
Gaston Jaurena <gassjau@gmail.com>
2023-09-11 14:14:15 -0300

    Estrellitas

--- js/product-info.js
+++ js/product-info.js
@@ -4,4 +4,79 @@ document.addEventListener("DOMContentLoaded",()=>{
         let emailPersona = document.getElementById("emailPersona");
         emailPersona.innerHTML = `Perfil: ${contenidoIndex}`;
 
+
+        const estrellita1 = document.getElementById("estrellita1")
+
+        estrellita1.addEventListener("click", ()=> {
+
+                estrellita1.classList.add("claseEstrella")
+                estrellita2.classList.add("claseEstrellaWhite")
+                estrellita3.classList.add("claseEstrellaWhite")
+                estrellita4.classList.add("claseEstrellaWhite")
+                estrellita5.classList.add("claseEstrellaWhite")
+                
+                
+        })
+
+        const estrellita2 = document.getElementById("estrellita2")
+
+        estrellita2.addEventListener("click", ()=> {
+
+                estrellita1.classList.add("claseEstrella")
+                estrellita2.classList.add("claseEstrella")
+                estrellita2.classList.remove("claseEstrellaWhite")
+
+                estrellita3.classList.add("claseEstrellaWhite")
+                estrellita4.classList.add("claseEstrellaWhite")
+                estrellita5.classList.add("claseEstrellaWhite")
+                
+
+        })
+        const estrellita3 = document.getElementById("estrellita3")
+
+        estrellita3.addEventListener("click", ()=> {
+
+                estrellita1.classList.add("claseEstrella")
+                estrellita2.classList.add("claseEstrella")
+                estrellita3.classList.add("claseEstrella")
+                estrellita2.classList.remove("claseEstrellaWhite")
+                estrellita3.classList.remove("claseEstrellaWhite")
+                estrellita4.classList.add("claseEstrellaWhite")
+                estrellita5.classList.add("claseEstrellaWhite")
+                
+        })
+
+        const estrellita4 = document.getElementById("estrellita4")
+
+        estrellita4.addEventListener("click", ()=> {
+
+                estrellita1.classList.add("claseEstrella")
+                estrellita2.classList.add("claseEstrella")
+                estrellita3.classList.add("claseEstrella")
+                estrellita4.classList.add("claseEstrella")
+                estrellita2.classList.remove("claseEstrellaWhite")
+                estrellita3.classList.remove("claseEstrellaWhite")
+                estrellita4.classList.remove("claseEstrellaWhite")
+                estrellita5.classList.add("claseEstrellaWhite")
+               
+                
+        })
+        const estrellita5 = document.getElementById("estrellita5")
+
+        estrellita5.addEventListener("click", ()=> {
+
+                estrellita5.classList.add("claseEstrella")
+                estrellita4.classList.add("claseEstrella")
+                estrellita3.classList.add("claseEstrella")
+                estrellita2.classList.add("claseEstrella")
+                estrellita1.classList.add("claseEstrella")
+                estrellita2.classList.remove("claseEstrellaWhite")
+                estrellita3.classList.remove("claseEstrellaWhite")
+                estrellita4.classList.remove("claseEstrellaWhite")
+                estrellita5.classList.remove("claseEstrellaWhite")
+                
+                
+        })
+
+
 })
