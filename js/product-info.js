5a13c3aebb22186c8c57c2a1fad987225fb73070
manuromeyer <manurodriguez2015.11@gmail.com>
2023-09-12 14:51:12 -0300

    puntaje

--- js/product-info.js
+++ js/product-info.js
@@ -71,9 +71,9 @@ document.addEventListener("DOMContentLoaded", () => {
     .then((response) => response.json())
     .then(
       (data) =>
-        (
-          
-          contenidoDePag.innerHTML = `
+      (
+
+        contenidoDePag.innerHTML = `
                 
         <div>
         <h1 class="contenedor1">${data.name}</h1>
@@ -102,6 +102,77 @@ document.addEventListener("DOMContentLoaded", () => {
         </div>
         </div>
         `
-        )
+      )
     );
-});
+
+    function calificacion() {
+      if (data[i].score = "5") {
+        data[i].score.innerHTML = `
+        <i class="fas fa-star"></i>
+    <i class="fas fa-star"></i>
+    <i class="fas fa-star"></i>
+    <i class="fas fa-star"></i>
+    <i class="fas fa-star"></i>
+    `
+      }
+      else if (data[i].score = "4") {
+        data[i].score.innerHTML = `
+        <i class="fas fa-star"></i>
+    <i class="fas fa-star"></i>
+    <i class="fas fa-star"></i>
+    <i class="fas fa-star"></i>
+    <i class="far fa-star"></i>
+    `
+      }
+      else
+  if (data[i].score = "3") {
+        data[i].score.innerHTML = `
+        <i class="fas fa-star"></i>
+    <i class="fas fa-star"></i>
+    <i class="fas fa-star"></i>
+    <i class="far fa-star"></i>
+    <i class="far fa-star"></i>
+    `
+      }
+      else if (data[i].score = "2") {
+        data[i].score.innerHTML = `
+        <i class="fas fa-star"></i>
+    <i class="fas fa-star"></i>
+    <i class="far fa-star"></i>
+    <i class="far fa-star"></i>
+    <i class="far fa-star"></i>
+    `
+      }
+      else if (data[i].score = "1") {
+  data[i].score.innerHTML = `
+        <i class="fas fa-star"></i>
+        <i class="far fa-star"></i>
+        <i class="far fa-star"></i>
+        <i class="far fa-star"></i>
+    <i class="far fa-star"></i>
+    `
+      }
+    }
+
+
+  let comments = PRODUCT_INFO_COMMENTS_URL + numeroProd + ".json";
+  let infocomentarios = document.querySelector("#comments");
+  fetch(comments)
+    .then((response) => response.json())
+    .then(
+      (data) =>
+      (
+
+        infocomentarios.innerHTML = `
+        <div class="container"> 
+        <h4>Comentarios</h4>
+        <ul class="list-group">
+        <li class="list-group-item"><strong>${data[0].user}</strong><p>${data[0].dateTime}</p> <p>${data[0].score} </p> <p>${data[0].description}</p></li>
+        <li class="list-group-item"><strong>${data[1].user}</strong><p>${data[1].dateTime}</p> <p>${data[1].score} </p>  <p>${data[1].description}</p></li>
+        <li class="list-group-item"><strong>${data[2].user}</strong><p>${data[2].dateTime}</p> <p>${data[2].score} </p>  <p>${data[2].description}</p></li>
+        <li class="list-group-item"><strong>${data[3].user}</strong><p>${data[3].dateTime}</p> <p>${data[3].score} </p>  <p>${data[3].description}</p></li>
+        
+      </ul> 
+        </div>`
+        
+));})
