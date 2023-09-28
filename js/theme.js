58762ee3c7b3f72125fcedf029a51e5323b6186b
Lucia Machado <lucia.machado.olascoaga@gmail.com>
2023-09-28 15:32:32 -0300

    Modo negro en el localStorage

--- js/theme.js
+++ js/theme.js
@@ -1,19 +1,33 @@
 const temaOscuro = () => {
-    console.log("Tema oscuro activado");
-    document.querySelector("body").setAttribute("data-bs-theme", "dark");
-    document.querySelector("#dl-icon").setAttribute("class", "bi bi-sun-fill");
-  };
-  
-  const temaClaro = () => {
-    console.log("Tema claro activado");
-    document.querySelector("body").setAttribute("data-bs-theme", "light");
-    document.querySelector("#dl-icon").setAttribute("class", "bi bi-moon-fill");
-  };
-  
-  const cambiarTema = () => {
-    console.log("Cambio de tema");
-    document.querySelector("body").getAttribute("data-bs-theme") === "light"
-      ? temaOscuro()
-      : temaClaro();
-  };
-  
+  console.log("Tema oscuro activado");
+  document.querySelector("body").setAttribute("data-bs-theme", "dark");
+  document.querySelector("#dl-icon").setAttribute("class", "bi bi-sun-fill");
+
+  localStorage.setItem("modoPreferido", "oscuro");
+};
+
+const temaClaro = () => {
+  console.log("Tema claro activado");
+  document.querySelector("body").setAttribute("data-bs-theme", "light");
+  document.querySelector("#dl-icon").setAttribute("class", "bi bi-moon-fill");
+
+  localStorage.setItem("modoPreferido", "claro");
+};
+
+const cambiarTema = () => {
+  console.log("Cambio de tema");
+  if (document.querySelector("body").getAttribute("data-bs-theme") === "light") {
+    temaOscuro();
+  } else {
+    temaClaro();
+  }
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  const modoPreferido = localStorage.getItem("modoPreferido");
+  if (modoPreferido === "oscuro") {
+    temaOscuro();
+  } else {
+    temaClaro();
+  }
+});
