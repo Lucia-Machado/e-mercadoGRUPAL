ac3c13a361cb069ea674ea1fd1d5f01f578a331c
aniburgos <aburgosuru@gmail.com>
2023-09-28 16:56:36 -0300

    carrusel de imágenes

--- js/product-info.js
+++ js/product-info.js
@@ -89,22 +89,37 @@ document.addEventListener("DOMContentLoaded", () => {
         <p class="cat1">Cantidad vendidos:</p><p>${data.soldCount}</p>
         <p class="cat1">Imágenes: </p>
 
-        <div class="galeria">
-        <div class="imagenesgas">
-        <img src="${data.images[0]}"></img>
+
+        <div id="carousel" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-indicators">
+          <button type="button" data-bs-target="#carousel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+          <button type="button" data-bs-target="#carousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
+          <button type="button" data-bs-target="#carousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
+          <button type="button" data-bs-target="#carousel" data-bs-slide-to="3" aria-label="Slide 4"></button>
         </div>
-        <div class="imagenesgas">
-        <img src="${data.images[1]}"></img>
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img src="${data.images[0]}" class="d-block w-100" alt="...">
+          </div>
+          <div class="carousel-item">
+            <img src="${data.images[1]}" class="d-block w-100" alt="...">
+          </div>
+          <div class="carousel-item">
+            <img src="${data.images[2]}" class="d-block w-100" alt="...">
+          </div>
+          <div class="carousel-item">
+            <img src="${data.images[3]}" class="d-block w-100" alt="...">
+          </div>
         </div>
-        <div class="imagenesgas">
-        <img src="${data.images[2]}"></img>
-        </div>
-        <div class="imagenesgas">
-        <img src="${data.images[3]}"></img>
-        </div>
-              
-        </div>
-        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carousel" data-bs-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carousel" data-bs-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </div>
         `)
     );
 
