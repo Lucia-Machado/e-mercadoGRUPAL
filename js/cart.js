5d7433038f0bafed5a00e291d1343949173da100
Gaston Jaurena <gassjau@gmail.com>
2023-10-20 15:01:31 -0300

    eliminar de a un carrito

--- js/cart.js
+++ js/cart.js
@@ -12,22 +12,46 @@ function recalcular() {
   document.getElementById("total").innerHTML = "$ " + total;
 }
 
+let carrito2 = document.getElementById("lista2");
+function renderizarCarrito() {
+
+  carrito2.innerHTML = '';
+
+    carrito.map((el)=>{
+      carrito2.innerHTML += `
+      <tr><td><img src=${el.images[0]} width="100px"></img></td>
+      <td id="nombreCarrito">${el.name} </td>
+      <td><input type="number" id="inputCarrito" min="0" value="1" class="cant" onchange="recalcular()"></td>
+      <td id="ress">${el.currency} <span class="res">${el.cost}</span>
+      <button onclick="eliminarDelCarrito(${el.id})">Eliminar</button>
+      </td></tr><br/>`;
+    
+  });
+
+};
+function eliminarDelCarrito(id) {
+  let carritoD = carrito.filter(elem => elem.id != id)
+  localStorage.removeItem(carritoD)
+  carrito = carritoD
+  renderizarCarrito();
+}
+let carrito = JSON.parse (localStorage.getItem("carrito"));
 document.addEventListener("DOMContentLoaded", () => {
   const carritoCont = document.getElementById("lista");
-  const carrito = JSON.parse (localStorage.getItem("carrito"));
  
   
   const URL = "https://japceibal.github.io/emercado-api/user_cart/25801.json";
   ////
     
 
-   
   
     
 ////
   fetch(URL)
     .then((response) => response.json())
     .then((productocompra) => {
+
+      
       carritoCont.innerHTML += `
     <tr><td><img src=${productocompra.articles[0].image} width="100px"></img></td>
     <td id="nombreCarrito">${productocompra.articles[0].name} </td>
@@ -41,24 +65,29 @@ document.addEventListener("DOMContentLoaded", () => {
     <tr><td><img src=${el.images[0]} width="100px"></img></td>
     <td id="nombreCarrito">${el.name} </td>
     <td>${el.currency}<span class="precio"> ${el.cost}</span></td>
-    <td><input type="number" id="inputCarrito" min="0" value="1" class="cant" onchange="recalcular()"></td>
-    <td id="ress">${el.currency} <span class="res">${el.cost}</span></td></tr><br/>`;
-      })
-      
     
-      recalcular();
-    });
+    <td><input type="number" id="inputCarrito" min="0" value="1" class="cant" onchange="recalcular()"></td>
+    <td id="ress">${el.currency} <span class="res">${el.cost}</span>
+    <button onclick="eliminarDelCarrito(${el.id})">Eliminar</button></td>
+    </tr><br/>
+    `;
+  })
+  
+
+  
+  
+  recalcular();
 
 
     const limpiar = document.getElementById("limpiarProd")
 
     limpiar.addEventListener("click", ()=>{
       localStorage.removeItem("carrito")
-      let carrito2 = document.getElementById("lista2");
+      
       carrito2.innerHTML = ` `
       let carritoCont = document.getElementById("lista");
       carritoCont.innerHTML = ` `
     })
   
 })
-
+})
