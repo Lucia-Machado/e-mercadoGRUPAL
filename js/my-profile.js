84b6fc29308002278b72beabf99fa8d52ccdd334
manuromeyer <manurodriguez2015.11@gmail.com>
2023-11-10 13:24:32 -0300

    primeraparte

---
+++ js/my-profile.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function(){
+
+    let nombre = document.querySelector("#txtNombre").value; 
+    let apellido = document.querySelector("#txtApellido").value; 
+    let email = document.querySelector("#txtEmail").value; 
+    let segundoNombre = document.querySelector("#txtSegundoNombre").value; 
+    let segundoApellido = document.querySelector("#txtSegundoApellido").value; 
+    let telefono = document.querySelector("#txtTelefono").value; 
+    })
