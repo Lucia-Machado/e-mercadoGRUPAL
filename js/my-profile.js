377fae23c4134b6ff55b70ae6b6395930d577515
Gaston Jaurena <gassjau@gmail.com>
2023-11-10 13:44:25 -0300

    agregoEmailEnInput

--- js/my-profile.js
+++ js/my-profile.js
@@ -2,19 +2,21 @@ document.addEventListener("DOMContentLoaded", function () {
 
     let nombre = document.querySelector("#txtNombre").value;
     let apellido = document.querySelector("#txtApellido").value;
-    let email = document.querySelector("#txtEmail").value;
+    let email = document.getElementById("txtEmail");
     let segundoNombre = document.querySelector("#txtSegundoNombre").value;
     let segundoApellido = document.querySelector("#txtSegundoApellido").value;
     let telefono = document.querySelector("#txtTelefono").value;
 
 
+    email.value = localStorage.getItem("EmailPersona")
+    console.log(email)
     if (!sessionStorage.getItem('loggedIn')) {
 
         // si no es asi, redirige a login hasta que sea true
         window.location.href = 'login.html';
     }
     let contenidoIndex = localStorage.getItem("EmailPersona");
-    let emailPersona = document.getElementById("emailPersona");
-    emailPersona.innerHTML = `Perfil: ${contenidoIndex}`;
+    // let emailPersona = document.getElementById("emailPersona");
+    // emailPersona.innerHTML = `Perfil: ${contenidoIndex}`;
 
 })
