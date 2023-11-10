fffb036f1c1dffa1af272879a8dd9c5b88a64d1a
Gaston Jaurena <gassjau@gmail.com>
2023-11-10 14:19:51 -0300

    agregoLOcalStoragePerfil

--- js/my-profile.js
+++ js/my-profile.js
@@ -14,9 +14,42 @@ document.addEventListener("DOMContentLoaded", function () {
 
         // si no es asi, redirige a login hasta que sea true
         window.location.href = 'login.html';
+
+
     }
     let contenidoIndex = localStorage.getItem("EmailPersona");
-    // let emailPersona = document.getElementById("emailPersona");
-    // emailPersona.innerHTML = `Perfil: ${contenidoIndex}`;
+    let emailPersona = document.getElementById("emailPersona");
+    const navlink = document.getElementsByClassName("nav-link")
+
+    navlink.innerHTML = emailPersona
+    emailPersona.innerHTML = `Perfil: ${contenidoIndex}`;
+
+
+    const btnValidar = document.getElementById("btnValidar")
+    btnValidar.addEventListener("click", () => {
+        let nombre = document.querySelector("#txtNombre").value;
+        let apellido = document.querySelector("#txtApellido").value;
+        let email = document.getElementById("txtEmail").value;
+        let segundoNombre = document.querySelector("#txtSegundoNombre").value;
+        let segundoApellido = document.querySelector("#txtSegundoApellido").value;
+        let telefono = document.querySelector("#txtTelefono").value;
+        let emailPersona = document.getElementById("emailPersona").value;
+        
+        localStorage.setItem("nombre", JSON.stringify(nombre))
+        localStorage.setItem("apellido", JSON.stringify(apellido))
+        localStorage.setItem("EmailPersona", JSON.stringify(email))
+
+        if (segundoNombre){
+            console.log("validacion")
+            localstorage.setItem("validarSegundoNombre", JSON.stringify(segundoNombre))
+        }
+        // if(segundoApellido){
+
+        //     localStorage.setItem("validarSegundoApellido", )
+        // }
+        // if(telefono){
+        //     localStorage.setItem("validarTelefono", )
+        // }
+    })
 
 })
