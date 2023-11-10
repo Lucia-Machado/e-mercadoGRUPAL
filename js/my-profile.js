cc278312244c6039792430dbae6614fe31e0374f
manuromeyer <manurodriguez2015.11@gmail.com>
2023-11-10 13:33:35 -0300

    jsPerfil

--- js/my-profile.js
+++ js/my-profile.js
@@ -1,9 +1,20 @@
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", function () {
 
-    let nombre = document.querySelector("#txtNombre").value; 
-    let apellido = document.querySelector("#txtApellido").value; 
-    let email = document.querySelector("#txtEmail").value; 
-    let segundoNombre = document.querySelector("#txtSegundoNombre").value; 
-    let segundoApellido = document.querySelector("#txtSegundoApellido").value; 
-    let telefono = document.querySelector("#txtTelefono").value; 
-    })
+    let nombre = document.querySelector("#txtNombre").value;
+    let apellido = document.querySelector("#txtApellido").value;
+    let email = document.querySelector("#txtEmail").value;
+    let segundoNombre = document.querySelector("#txtSegundoNombre").value;
+    let segundoApellido = document.querySelector("#txtSegundoApellido").value;
+    let telefono = document.querySelector("#txtTelefono").value;
+
+
+    if (!sessionStorage.getItem('loggedIn')) {
+
+        // si no es asi, redirige a login hasta que sea true
+        window.location.href = 'login.html';
+    }
+    let contenidoIndex = localStorage.getItem("EmailPersona");
+    let emailPersona = document.getElementById("emailPersona");
+    emailPersona.innerHTML = `Perfil: ${contenidoIndex}`;
+
+})
